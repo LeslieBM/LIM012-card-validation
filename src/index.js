3b9bf9a1301864b7d63f16f70f783524c88af122
LeslieBM <lesliebenelmendoza@gmail.com>
2020-02-19 13:24:20 -0500

    Avance hasta validator y maskify

--- src/index.js
+++ src/index.js
@@ -2,95 +2,34 @@ import validator from './validator.js';
     
     console.log(validator);
 
-// Inicia la sección
-const enter = document.getElementById("button1");
-const sectionCard = document.getElementById("entercard");
-sectionCard.classList.add("ocultar");
+// Sección Inicial
+const buttonEnter = document.getElementById("button1");
+const sectionEnterCard = document.getElementById("entercard");
 
+// Mostrar Sección de Ingresar Datos de tarjeta
 const startSection = document.getElementById("startpage");
-enter.addEventListener("click",start,false);
+buttonEnter.addEventListener("click",start);
 function start(){
   startSection.classList.add("ocultar");
-  sectionCard.classList.add("mostrar");
+  sectionEnterCard.classList.remove("ocultar");
 };
-// Solicitar Número de la Tarjeta
-function getCreditCardNumber() {
-  let numberCard = document.getElementById("creditcard").value;
-  let invertedNumber = "";
-//Invertir Números de la tarjeta
-  for(let i= numberCard.length-1 ; i >= 0; i--){
-    invertedNumber = invertedNumber + numberCard[i];
-    }
-    //Convertir a array y a números
-    const newArray = Array.from(invertedNumber).map(Number);
-    //console.log("Nros invertidos",newArray)
-    //Multiplicar por 2 las posiciones pares
-    let numberCardList = [];
 
-    for(let i=0; i < newArray.length; i++){
-      if(i%2!==0){
-        numberCardList.push(newArray[i]*2);
-      }else{
-        numberCardList.push(newArray[i]*1);
-      }
-    }
-    //console.log("Pares multiplicados por 2", numberCardList)
+//let numberCard2 = document.getElementById("creditcard").value;
+const result = document.getElementById("result");
+document.getElementById("button2").addEventListener("click",cardValidation);
 
-    //Sumar los dígitos de los nros. mayores que 9 de las posiciones pares 
-    let numberCardList2 = [];
-
-    for(let i=0; i < numberCardList.length; i++){
-      if(numberCardList[i]>=10){
-        numberCardList2.push((numberCardList[i]-10)+1);
-      }else{
-        numberCardList2.push(numberCardList[i]*1)
-      }
-    }
-    //console.log("Sumar dígitos de nros mayores que 10",numberCardList2);
-
-    //Sumar todos los números
-    let numberCardSum = numberCardList2.reduce((sum,actual)=>sum+actual,0);
-    //console.log("suma",numberCardSum)
-
-    //Validar Tarjeta de Crédito
-    let validation = false;
-    if(numberCardSum%10===0){
-        validation = true;
-      }
-      console.log(validation);
-}
-
-//Enmascarar los números
-function maskify(){
-  
-  let numberCard2 = document.getElementById("creditcard").value;
-  let numbers = "";
-
-  for(let i=0; i<numberCard2.length; i++){
-
-    if(i<= numberCard2.length - 5 ){
-      numbers = numbers + "#";
-    }else{
-      numbers = numbers + numberCard2[i]*1;
-    } document.getElementById("validation").innerText = numbers;
-  } 
-}
-
-  const button = document.getElementById("button2");
-  button.addEventListener("click",getCreditCardNumber,false);
-  button.addEventListener("click",maskify,false);
-
-  const validate = document.getElementById("validation");
-  validate.classList.add("ocultar");
- 
-  button.addEventListener("click",next,false);
-  function next(){
-    validate.classList.add("mostrar");
-    sectionCard.classList.add("ocultar");
-  }
+function cardValidation(){
+  let todo = validator.isValid(document.getElementById("creditcard").value);
+  let todo2 = validator.maskify(document.getElementById("creditcard").value);
+  sectionEnterCard.classList.add("ocultar");
+  let validate = document.getElementById("validation");
+  validate.classList.remove("ocultar");
+  result.innerHTML= todo + " " +todo2;
+};
 
 
-    
+
+
   
   
 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,69 @@
 const validator = {
-  // ...
+};
+// Solicitar Número de la Tarjeta
+validator.isValid = function(numberCard) {
+  let invertedNumber = "";
+  //Invertir Números de la tarjeta
+  for(let i= numberCard.length-1 ; i >= 0; i--){
+    invertedNumber = invertedNumber + numberCard[i];
+    }
+    //Convertir a array y a números
+    const newArray = Array.from(invertedNumber).map(Number);
+    console.log("Nros invertidos",newArray)
+    //Multiplicar por 2 las posiciones pares
+    let numberCardList = [];
+
+    for(let i=0; i < newArray.length; i++){
+      if(i%2!==0){
+        numberCardList.push(newArray[i]*2);
+      }else{
+        numberCardList.push(newArray[i]*1);
+      }
+    }
+    console.log("Pares multiplicados por 2", numberCardList)
+
+    //Sumar los dígitos de los nros. mayores que 9 de las posiciones pares 
+    let numberCardList2 = [];
+
+    for(let i=0; i < numberCardList.length; i++){
+      if(numberCardList[i]>=10){
+        numberCardList2.push((numberCardList[i]-10)+1);
+      }else{
+        numberCardList2.push(numberCardList[i]*1)
+      }
+    }
+    console.log("Sumar dígitos de nros mayores que 10",numberCardList2);
+
+    //Sumar todos los números
+    let numberCardSum = numberCardList2.reduce((sum,actual)=>sum+actual,0);
+    console.log("suma",numberCardSum)
+
+    //Validar Tarjeta de Crédito
+    let validation = false;
+    if(numberCardSum%10===0){
+        validation = true;
+      }
+      console.log(validation);
+      if(validation==true){
+        return "tarjeta valida";
+      }
+      else{
+        return "tarjeta invalida"
+      }
+};
+
+
+//Enmascarar los números
+validator.maskify = function (numberCard2){
+  let numbers = "";
+  for(let i=0; i<numberCard2.length; i++){
+    if(i<= numberCard2.length - 5 ){
+      numbers = numbers + "#";
+    }else{
+      numbers = numbers + numberCard2[i];
+    }
+  } 
+  return numbers
 };
 
 export default validator;
